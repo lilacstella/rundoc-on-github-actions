84080f1ed23a32cc21bbb0d10392e08d4688e28b
lilacstella <43301578+lilacstella@users.noreply.github.com>
2024-07-08 18:42:53 +0000

    run rundoc

---
+++ .rundoc-workspace/vendor/bundle/ruby/3.3.0/gems/rundoc-2.0.1/lib/rundoc/version.rb
@@ -0,0 +1,3 @@
+module Rundoc
+  VERSION = "2.0.1"
+end
---
+++ .rundoc-workspace/vendor/bundle/ruby/3.3.0/gems/rundoc-2.0.1/test/test_helper.rb
@@ -0,0 +1,11 @@
+require "bundler"
+
+Bundler.require
+
+require "rundoc"
+require "minitest/autorun"
+require "mocha/minitest"
+require "tmpdir"
+
+def assert_tests_run
+end
